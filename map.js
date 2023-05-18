1d8fb0e36c29711820eb5c37fbdfa1204f132528
mikavyas <133055847+mikavyas@users.noreply.github.com>
2023-05-18 17:09:10 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!")
+'use strict'  // execute in strict mode
+
+console.log("Loaded map.js")
+
+// your mapbox token
+mapboxgl.accessToken = 'pk.eyJ1IjoibWlrYXZ5YXMiLCJhIjoiY2xoczhzOTUzMHpreTNlbjAweXJ3OWRxMyJ9.W3Lh5-pynBUC4YNHA3vQkg'
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v9',
+    center: [-73.93324, 40.80877],
+    zoom: 14
+});
